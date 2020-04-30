d28a2e39ac135380a6c9efbc480a35026e80d6a7
Samuel Lotz <samuel.lotz@salotz.info>
2020-04-30 11:18:42 -0400

    python init for versioneer

--- src/openmm_systems/__init__.py
+++ src/openmm_systems/__init__.py
@@ -6,3 +6,7 @@ __author__ = "Samuel D. Lotz"
 __email__ = 'samuel.lotz@salotz.info'
 
 ## versioneer code will go here:
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
